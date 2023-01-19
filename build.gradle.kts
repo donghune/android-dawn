0fd4cc2493434d930a72280abbf4950c51f3c527
donghune <reona7140@naver.com>
2023-01-19 16:30:10 +0900

    build: project:build.gradle groovy -> kotlin 변경

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("com.android.application") version "7.4.0" apply false
+    kotlin("android") version "1.7.0" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint") // Version should be inherited from parent
+
+    repositories {
+        // Required to download KtLint
+        mavenCentral()
+    }
+
+    // Optionally configure plugin
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        debug.set(true)
+    }
+}
