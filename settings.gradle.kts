f4446f0a9d60c043019f0203483fa0b173cfdebd
donghune <reona7140@naver.com>
2023-01-31 19:07:42 +0900

    fix: app 모듈 외 나머지 삭제

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,8 +18,3 @@ rootProject.name = "dawn"
 rootProject.buildFileName = "build.gradle.kts"
 
 include(":app")
-include(":common:network")
-include(":common:design-system")
-include(":data:example")
-include(":feature:example")
-include(":domain:example")
